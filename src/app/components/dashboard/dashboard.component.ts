3603069bfc5fd0f66d52ab01ffaad2bf20d3790b
bc_wallet_220907_zfehija066 <bc_wallet_220907_zfehija066@BuilderCloudProvisioning.onmicrosoft.com>
2022-10-15 18:01:39 +0530

    add ui

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -12,4 +12,15 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  title = 'e-AuctionApp';
+
+  options: any = [
+    { Id: 1, Value: 'Product 1' },
+    { Id: 2, Value: 'Product 2' },
+    { Id: 3, Value: 'Product 3' },
+    { Id: 4, Value: 'Product 4' },
+    { Id: 5, Value: 'Product 5' },
+    { Id: 6, Value: 'Product 6' },
+  ];
+
 }
